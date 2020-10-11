4ad62c391f24b0bef9080e23dff54b1ec99a0ca1
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2020-10-11 00:22:57 -0500

    Fixing dotenv

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'dotenv/load'
-
 module RailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
